1ccb1859dcbe41fe485ba82ec60c5cc28fe6da6f
Kyle Bailey <kylealexbailey@gmail.com>
2021-07-19 22:32:07 +0900

    add allrecipes keyword scraping function

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'pry-byebug'
 require 'better_errors'
 require_relative 'cookbook'
 require_relative 'recipe'
+require_relative 'scrape_all_recipes_service'
 
 configure :development do
   use BetterErrors::Middleware
@@ -11,6 +12,8 @@ configure :development do
 end
 
 cookbook = Cookbook.new('./recipes.csv')
+URL = "https://www.allrecipes.com/search/results/?search="
+searched = []
 
 get '/' do
   @recipes = cookbook.all
@@ -22,8 +25,7 @@ get '/recipes/new' do
 end
 
 post '/recipes/create' do
-  args = params.to_h.transform_keys(&:to_sym)
-  recipe = Recipe.new(**args)
+  recipe = Recipe.new(**params)
   cookbook.add_recipe(recipe)
   redirect to('/')
 end
@@ -31,11 +33,25 @@ end
 # localhost:4567/recipes/delete/1
 # params => { "id" => '1' }
 get '/recipes/delete/:id' do
-  cookbook.remove_recipe(params['id'].to_i)
+  cookbook.remove_recipe(params[:id].to_i)
   redirect to('/')
 end
 
 get '/recipes/complete/:id' do
-  cookbook.mark_recipe_done(params['id'].to_i)
+  cookbook.mark_recipe_done(params[:id].to_i)
   redirect to('/')
 end
+
+get '/recipes/search' do
+  @keyword = params[:keyword]
+  @recipes = ScrapeAllRecipesService.retrieve(URL + params[:keyword])
+  searched = @recipes
+  erb :results
+end
+
+# get '/recipes/import/:id' do
+#   binding.pry
+#   recipe = searched[params[:id].to_i]
+#   cookbook.add_recipe(recipe)
+#   redirect to('/')
+# end
--- scrape_all_recipes_service.rb
+++ scrape_all_recipes_service.rb
@@ -3,20 +3,25 @@ require_relative 'recipe'
 require 'open-uri'
 
 class ScrapeAllRecipesService
-  def self.retrieve(url)
-    file = Nokogiri::HTML(URI.open(url).read, nil, 'utf-8')
-    file.search('.card__detailsContainer').first(5).map do |recipe|
-      title = recipe.search('.card__title').text.strip
-      description = recipe.search('.card__summary').text.strip
-      rating = recipe.search('.review-star-text').text.strip.match(/\d\.?\d?\d?/)
-      prep_time = fetch_prep(recipe.search('.card__titleLink').attribute('href').value)
-      Recipe.new(title, description, prep_time, rating)
+  class << self
+    def retrieve(url)
+      file = Nokogiri::HTML(URI.open(url).read, nil, 'utf-8')
+      file.search('.card__detailsContainer').first(5).map do |recipe|
+        args = {}
+        args[:name] = recipe.search('.card__title').text.strip
+        args[:description] = recipe.search('.card__summary').text.strip
+        args[:rating] = recipe.search('.review-star-text').text.strip.match(/\d\.?\d{,2}/)
+        args[:prep_time] = fetch_prep(recipe.search('.card__titleLink').attribute('href').value)
+        Recipe.new(**args)
+      end
     end
-  end
 
-  def self.fetch_prep(url)
-    file = Nokogiri::HTML(URI.open(url).read, nil, 'utf-8')
-    file.search('.recipe-meta-item-body').first.text.strip
+    private
+
+    def fetch_prep(url)
+      file = Nokogiri::HTML(URI.open(url).read, nil, 'utf-8')
+      file.search('.recipe-meta-item-body').first.text.strip
+    end
   end
 end
 
